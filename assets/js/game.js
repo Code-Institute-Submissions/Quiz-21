0c07f4190b8b011d27c2ef4ad9061556c06871f1
Mykyta Levskyi <nikita.levskiy@gmail.com>
2022-10-30 08:36:20 +0000

    game basic structure

---
+++ assets/js/game.js
@@ -0,0 +1,23 @@
+function runGame() {
+    
+}
+
+function checkAnswer(choice) {
+    
+}
+
+function incrementScore() {
+    
+}
+
+function incrementWrongAnswer() {
+    
+}
+
+function displayQuestion() {
+    
+}
+
+function endOfGame() {
+    
+}
