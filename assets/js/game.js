13bf46b6d76be14a53e7f4576426f3c65082dd27
Mykyta Levskyi <nikita.levskiy@gmail.com>
2022-10-30 08:38:15 +0000

    add questions and global variables

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,123 @@
+let questions = [
+    {
+        question: "How can be initialized a static array since C++ 11?",
+        choice: [
+            "std::array<int, 3> num{1, 2, 3}",
+            "std::array<3, int> num{1, 2, 3}",
+            "std::array<int, 1> num{1, 2, 3}",
+            "int array{1, 2, 3}"
+        ],
+        answer: 1,
+        score: 0.5
+    },
+    {
+        question: "Which library is used to determine a dynamic array?",
+        choice: [
+            "array",
+            "string",
+            "vector",
+            "iostream"
+        ],
+        answer: 3,
+        score: 0.5
+    },
+    {
+        question: "Which variable declaration is wrong?",
+        choice: [
+            "int i{1}",
+            "int i = 1",
+            "bool i = true",
+            "let i = 1"
+        ],
+        answer: 4,
+        score: 0.5
+    },
+    {
+        question: "What is a pointer?",
+        choice: [
+            "a variable that stores the memory address as its value",
+            "a variable that stores value",
+            "an array member that stores the memory address as its value",
+            "a variable that points to the end of an array"
+        ],
+        answer: 1,
+        score: 2
+    },
+    {
+        question: "Which method is used in C++ as end of string?",
+        choice: [
+            "/n",
+            "std::endl",
+            "/t",
+            "endl"
+        ],
+        answer: 2,
+        score: 1
+    },
+    {
+        question: "All members of a structure by default are...",
+        choice: [
+            "protected",
+            "private",
+            "public",
+            "float"
+        ],
+        answer: 3,
+        score: 1
+    },
+    {
+        question: "All members of a class by default are...",
+        choice: [
+            "public",
+            "protected",
+            "double",
+            "private"
+        ],
+        answer: 4,
+        score: 0.5
+    },
+    {
+        question: "How can we access a member age of a Person structure?",
+        choice: [
+            "Person[age]",
+            "Person.age",
+            "Person->age",
+            "Person.[age]"
+        ],
+        answer: 2,
+        score: 2
+    },
+    {
+        question: "What are the basic concepts of OOP?",
+        choice: [
+            "class, object, inheritance and polymorphism",
+            "inheritance and polymorphism",
+            "encapsulation, inheritance and polymorphism",
+            "encapsulation, abstraction, inheritance and polymorphism"
+        ],
+        answer: 4,
+        score: 1
+    },
+    {
+        question: "How can we declare a reference variable?",
+        choice: [
+            "int& ref = x",
+            "&int ref = x",
+            "int&& ref = x",
+            "int ref& = x"
+        ],
+        answer: 1,
+        score: 1
+    }
+];
+
+const questionAmount = 10;
+let questionCounter = 0;
+let rightAnswers = 0;
+let wrongAnswers = 0;
+let index;
+let score = 0;
+
 function runGame() {
     
 }
